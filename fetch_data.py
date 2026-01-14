93cce4fb8d5e9fa7a4c73da4fcf9d8107334669b
option4futures1 <raju_eee243@rediffmail.com>
2026-01-14 08:34:16 +0530

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -17,10 +17,10 @@ API_KEY = os.getenv("API_KEY")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 
 EXPIRIES = [
-    ("2026-01-13", "Expiry1"),
-    ("2026-01-20", "Expiry2"),
-    ("2025-01-27", "Expiry3"),
-    ("2026-02-03", "Expiry4"),
+    ("2026-01-20", "Expiry1"),
+    ("2026-01-27", "Expiry2"),
+    ("2025-02-03", "Expiry3"),
+    ("2026-02-10", "Expiry4"),
 ]
 
 # -----------------------------
